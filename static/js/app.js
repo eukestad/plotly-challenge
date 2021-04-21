b66203322210caf5ae5b63405f7bce63ceaa113f
eukestad <69314608+eukestad@users.noreply.github.com>
2021-04-21 01:22:08 -0500

    saving bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -8,11 +8,15 @@ var names = incomingData.names;
 
 var demos = incomingData.metadata;
 
-// populate drop down
+
+// set variables for elements
+var demoPanel = d3.select("#sample-metadata");
+
 var idDropdown = d3.select('#selDataset');
 
 var idNumber = idDropdown.property("value");
 
+// populate drop down
 names.forEach((item) => {
     idDropdown.append("option").text(item).property("value", item);
   });
@@ -35,24 +39,25 @@ function optionChanged(idNumber) {
     demoData = filterDemos(idFilter);
     bellyData = filterSamples(idNumber);
 
-
-    // demoData = demos.filter(demo => demo.id === idNumber)
-    console.log(demoData);
+    
 
     fillDemos(demoData);
 
     plotBar(bellyData);
    
-
-    
-
-    
+    plotBubble(bellyData);    
 
 };
 
+function clearDemos() {
+    demoPars = d3.selectAll('#demo-line');
+    demoPars.remove();
+}
+
 // populate demographics
 function fillDemos(dataSet) {
-    var demoPanel = d3.select("#sample-metadata");
+    clearDemos();
+
     var demoList = [];
 
     Object.entries(dataSet[0]).forEach(([key,value]) => {
@@ -60,9 +65,8 @@ function fillDemos(dataSet) {
     });
     console.log(demoList)
 
-
     demoList.forEach((item) => {
-        demoPanel.append("p").text(item);
+        demoPanel.append("p").text(item).attr('id','demo-line');
       });
 };
 
@@ -73,18 +77,45 @@ function plotBar(dataSet) {
     var labels = dataSet.map(l => l.otu_ids)[0].slice(0,10).map(v => `OTU ${v}`);
     var hoverText = dataSet.map(t => t.otu_labels)[0].slice(0,10);
 
-    traceHBar = {
+    var traceHBar = {
         x: values,
         y: labels,
         text: hoverText,
         type: "bar",
         orientation: 'h'
     };
+
+    //@TODO Layout
     
-    barData = [traceHBar]
+    var barData = [traceHBar]
     console.log(barData);
     
     Plotly.newPlot('bar', barData);
+};
+
+// plot bubble chart
+function plotBubble(dataSet) {
+
+    var xValues = dataSet.map(l => l.otu_ids)[0];
+    var yValues = dataSet.map(v => v.sample_values)[0];
+    var markerSize = yValues;
+    var markerColor = xValues;
+    var textValues = dataSet.map(t => t.otu_labels)[0];
+
+    var traceBubble = {
+        x: xValues,
+        y: yValues,
+        marker: {
+            color: markerColor,
+            size: markerSize
+        },
+        mode:'markers'
+        
+    };
+
+    bubbleData = [traceBubble]
+
+    Plotly.newPlot('bubble', bubbleData);
 };
 
 
