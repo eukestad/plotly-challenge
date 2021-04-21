cba57f39f5b095268c0646bd92ef854ff60c72d6
eukestad <69314608+eukestad@users.noreply.github.com>
2021-04-21 00:45:15 -0500

    saving bar chart

---
+++ static/js/app.js
@@ -0,0 +1,86 @@
+// bring in data
+const incomingData = bbData;
+
+// set data subsets
+var samples = incomingData.samples;
+
+var names = incomingData.names;
+
+var demos = incomingData.metadata;
+
+// filter functions
+function filterSamples (idNumber) {
+    return samples.filter(sample => sample.id === idNumber);
+};
+
+function filterDemos (idNumber) {
+    return demos.filter(demo => demo.id === idNumber);
+};
+
+// populate drop down
+var idDropdown = d3.select('#selDataset')
+
+var idNumber = idDropdown.property("value");
+
+names.forEach((item) => {
+    idDropdown.append("option").text(item).property("value", item);
+  });
+
+//handle events
+function optionChanged(idNumber) {
+    console.log(idNumber);
+    bellyData = filterSamples(idNumber);
+    demoData = filterDemos(idNumber);
+    console.log(demoData);
+
+    fillDemos(demoData);
+
+    plotBar(bellyData);
+   
+
+    
+
+    
+
+};
+
+// populate demographics
+function fillDemos(dataSet) {
+    var demoPanel = d3.select("#sample-metadata");
+    var demoList = [];
+
+    Object.entries(dataSet).forEach(([key,value]) => {
+        demoList.push(`${key}: ${value}`);
+    });
+    console.log(demoList)
+
+
+    demoList.forEach((item) => {
+        demoPanel.append("p").text(item);
+      });
+};
+
+// plot bar chart
+function plotBar(dataSet) {
+
+    var values = dataSet.map(v => v.sample_values)[0].slice(0,10);
+    var labels = dataSet.map(l => l.otu_ids)[0].slice(0,10).map(v => `OTU ${v}`);
+    var hoverText = dataSet.map(t => t.otu_labels)[0].slice(0,10);
+
+    traceHBar = {
+        x: values,
+        y: labels,
+        text: hoverText,
+        type: "bar",
+        orientation: 'h'
+    };
+    
+    barData = [traceHBar]
+    console.log(barData);
+    
+    Plotly.newPlot('bar', barData);
+};
+
+
+
+
